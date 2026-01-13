cd2e14fa4949b13fcea758fcccf10fa11822a94a
محمد احمد <muhammad.ahmad.github@gmail.com>
2026-01-13 21:54:15 +0500

    style: responsiveness

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -3,7 +3,13 @@ import './LandingPage.css';
 const LandingPage = () => {
   return (
     <div className="landing-container">
-      <h1 className="portfolio-text">portfolio</h1>
+      <h1 className="portfolio-text">
+        <span className="portfolio-full">portfolio</span>
+        <span className="portfolio-mobile">
+          <span className="portfolio-part">port</span>
+          <span className="portfolio-part">folio</span>
+        </span>
+      </h1>
       <p className="name-text">MUHAMMAD AHMAD BUTT</p>
     </div>
   );
